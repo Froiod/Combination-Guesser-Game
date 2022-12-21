e1dbe1fb45c3e329227686f04d3cbc6b43aac574
Paolo Guray <froiodpaolo@gmail.com>
2022-12-21 22:28:14 +0800

    added elements

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ let gamePlay = false
 button.addEventListener('click', () => {
   if (!gamePlay) {
     gamePlay = true
+    maker()
     button.innerHTML = "Check Combo"
   } else {
     console.log("checker")
   }
 })
+
+// creating elements
+function maker() {
+  for (x = 0; x < 4; x++) {
+    let el = document.createElement("input")
+    el.setAttribute('type', 'number')
+    el.max = 9
+    el.min = 0
+    el.size = 1
+    el.style.width = "50px"
+    el.classList.add('numb')
+    el.value = 0
+    el.order = x
+    gameArea.appendChild(el)
+    console.log(el)
+  }
+}
